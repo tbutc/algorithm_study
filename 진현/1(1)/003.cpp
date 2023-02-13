4be97361fc3ad2eda67b8789f7631b539ad82416
tbutc <wlsgus79622@naver.com>
2023-02-13 14:57:07 +0900

    김진현

--- 진현/1(1)/003.cpp
+++ 진현/1(1)/003.cpp
@@ -4,16 +4,16 @@ using namespace std;
 int main(void) {
 	int n; //수의 개수
 	int m; // 합을 구해야 하는 횟수
-	int arr[100000]={0}; // 구간합 배열
-	int sum[100000];
+	int arr[100001]={0}; // 구간합 배열
+	int sum[100001];
 	
 	cin >> n;
 	cin >> m;
 	
 	//for문 전 예외처리 1회
-	cin >> arr[0];
+	cin >> arr[1];
 	//배열 숫자 입력받기
-	for(int i=1 ; i<n ; i++){
+	for(int i=2 ; i<=n ; i++){
 	    cin >> arr[i]; 
 	    arr[i]+=arr[i-1];
 	}
@@ -21,7 +21,7 @@ int main(void) {
 	int a, b; // 구간을 입력받을 변수
 	for(int i=0; i<m ; i++){
 	    cin >> a >> b;
-	    sum[i] = arr[b-1]-arr[a-2];
+	    sum[i] = arr[b]-arr[a-1];
 	}
 	
 	for(int i=0; i<m ; i++){ //출력하기
--- 진현/1(1)/005.cpp
+++ 진현/1(1)/005.cpp
@@ -3,12 +3,17 @@
 using namespace std;
 
 int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
 	int n; //n개의 수
 	int m; //m으로 나누어떨어지도록
 	cin >> n;   cin >> m; //입력받기
 	
-    vector<int> arr(n,0);
-    int result=0; //구하고자 하는 결과값
+    vector<long> arr(n,0);
+	vector<long> C(m,0);
+    long result=0; //구하고자 하는 결과값
 	
 	cin >> arr[0]; //0 예외처리
 	for(int i=1;i<n;i++){
@@ -18,19 +23,15 @@ int main(void) {
 	for(int i=0;i<n;i++){
 	    arr[i]=arr[i]%m;
 	    if(arr[i]==0){result++;} // 나머지가 0이면 ++
+
+		C[arr[i]]++;
 	}
 	
 	for(int i=0;i<m;i++){
-	    int tmp=0; // 나머지가 같은 값 count
-	    for(int j=0;j<n;j++){
-	        if(arr[j]==i){
-	            tmp++;
-	        }
-	    }
-	    if(tmp>1){ // 같은 값이 2개 이상이면
-	        result+=tmp*(tmp-1)/2; //tmpC2
+		if(C[i]>0){ // 같은 값이 2개 이상이면
+	        result+=C[i]*(C[i]-1)/2; //tmpC2
 	    }
 	}
-    cout<<result;
+    cout<<result<<"\n";
 	return 0;
 }
